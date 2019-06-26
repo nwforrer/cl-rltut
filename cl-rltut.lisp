3d29e9118b968c9718d149228bd75fb11e9b3455
Nick Forrer <nwforrer@gmail.com>
2019-06-25 20:31:23 -0400

    Updates for tutorial.

--- cl-rltut.lisp
+++ cl-rltut.lisp
@@ -5,24 +5,21 @@
 (defparameter *screen-width* 80)
 (defparameter *screen-height* 50)
 
-(defparameter *player-x* (round (/ *screen-width* 2)))
-(defparameter *player-y* (round (/ *screen-height* 2)))
-
-(defun draw ()
+(defun draw (player-x player-y)
   (blt:clear)
-  (setf (blt:color) (blt:rgba 1.0 1.0 1.0)
-        (blt:cell-char *player-x* *player-y*) #\@)
+  (setf (blt:color) (blt:white)
+        (blt:cell-char player-x player-y) #\@)
   (blt:refresh))
 
 (defun handle-keys ()
-  (let ((action (make-hash-table)))
+  (let ((action nil))
     (blt:key-case (blt:read)
-                  (:up (setf (gethash :move action) (cons 0 -1)))
-                  (:down (setf (gethash :move action) (cons 0 1)))
-                  (:left (setf (gethash :move action) (cons -1 0)))
-                  (:right (setf (gethash :move action) (cons 1 0)))
-                  (:escape (setf (gethash :exit action) t))
-                  (:close (setf (gethash :exit action) t)))
+                  (:up (setf action (list :move (cons 0 -1))))
+                  (:down (setf action (list :move (cons 0 1))))
+                  (:left (setf action (list :move (cons -1 0))))
+                  (:right (setf action (list :move (cons 1 0))))
+                  (:escape (setf action (list :quit t)))
+                  (:close (setf action (list :quit t))))
     action))
 
 (defun config ()
@@ -33,13 +30,15 @@
 (defun main ()
   (blt:with-terminal
     (config)
-    (loop :do
-      (draw)
-      (let* ((action (handle-keys))
-             (move (gethash :move action))
-             (exit (gethash :exit action)))
-        (if exit
-            (return))
-        (when move
-            (incf *player-x* (car move))
-            (incf *player-y* (cdr move)))))))
+    (loop :with player-x = (/ *screen-width* 2)
+          :and player-y = (/ *screen-height* 2)
+          :do
+             (draw player-x player-y)
+             (let* ((action (handle-keys))
+                    (move (getf action :move))
+                    (exit (getf action :quit)))
+               (if exit
+                   (return))
+               (when move
+                 (incf player-x (car move))
+                 (incf player-y (cdr move)))))))
