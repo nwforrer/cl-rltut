a7c19a59424f6fb50c8c8909bebfe3fefb6c4880
Nick Forrer <nwforrer@gmail.com>
2019-07-01 23:24:51 -0400

    Finish generating rooms and tunnels.

--- cl-rltut.lisp
+++ cl-rltut.lisp
@@ -8,7 +8,9 @@
 (defparameter *map-width* 80)
 (defparameter *map-height* 45)
 
-(defparameter *map* nil)
+(defparameter *room-max-size* 10)
+(defparameter *room-min-size* 6)
+(defparameter *max-rooms* 30)
 
 (defparameter *color-map* (list :dark-wall (blt:rgba 0 0 100)
                                 :dark-ground (blt:rgba 50 50 150)))
@@ -60,31 +62,35 @@
   (blt:set "window.size = ~Ax~A" *screen-width* *screen-height*)
   (blt:set "window.title = Roguelike"))
 
+(defun game-tick (player entities map)
+  (render-all entities map)
+  (let* ((action (handle-keys))
+         (move (getf action :move))
+         (exit (getf action :quit)))
+    (when move
+      (unless (blocked-p map
+                         (+ (entity/x player) (car move))
+                         (+ (entity/y player) (cdr move)))
+        (move player (car move) (cdr move))))
+
+    exit))
+
 (defun main ()
   (blt:with-terminal
     (config)
-    (setf *map* (make-instance 'game-map :w *map-width* :h *map-height*))
-    (make-map *map*)
-    (loop :with player = (make-instance 'entity
-                                        :x (/ *screen-width* 2)
-                                        :y (/ *screen-height* 2)
-                                        :char #\@
-                                        :color (blt:white))
-          :and npc = (make-instance 'entity
-                                    :x (- (/ *screen-width* 2) 5)
-                                    :y (/ *screen-height* 2)
-                                    :char #\@
-                                    :color (blt:yellow))
-          :with entities = (list player npc)
-          :do
-             (render-all entities *map*)
-             (let* ((action (handle-keys))
-                    (move (getf action :move))
-                    (exit (getf action :quit)))
-               (when exit
-                 (return))
-               (when move
-                 (unless (blocked-p *map*
-                                    (+ (entity/x player) (car move))
-                                    (+ (entity/y player) (cdr move)))
-                   (move player (car move) (cdr move))))))))
+    (let* ((player (make-instance 'entity
+                                  :x (/ *screen-width* 2)
+                                  :y (/ *screen-height* 2)
+                                  :char #\@
+                                  :color (blt:white)))
+           (npc (make-instance 'entity
+                               :x (- (/ *screen-width* 2) 5)
+                               :y (/ *screen-height* 2)
+                               :char #\@
+                               :color (blt:yellow)))
+           (entities (list player npc))
+           (map (make-instance 'game-map :w *map-width* :h *map-height*)))
+      (make-map map *max-rooms* *room-min-size* *room-max-size* *map-width* *map-height* player)
+
+      (do ((exit nil (game-tick player entities map)))
+          (exit)))))
