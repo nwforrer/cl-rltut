e93eaeaa3ef440b3f2d8f2168843c6a024669ae5
Nick Forrer <nwforrer@gmail.com>
2019-10-03 07:40:03 -0400

    Fix monster death message using corpse name.

--- death-functions.lisp
+++ death-functions.lisp
@@ -8,10 +8,11 @@
 
 (defun kill-monster (monster)
   (with-slots (char color blocks ai name render-order) monster
-    (setf char #\%
-          color (blt:red)
-          blocks nil
-          ai nil
-          name (format nil "remains of ~A" name)
-          render-order :corpse)
-    (format nil "~A is dead!" name)))
+    (let ((message (format nil "~A is dead!~%" name)))
+      (setf char #\%
+            color (blt:red)
+            blocks nil
+            ai nil
+            name (format nil "remains of ~A" name)
+            render-order :corpse)
+      message)))
